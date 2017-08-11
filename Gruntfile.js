384fa7fbcc94e4fdc5c269ead00e62589dcf45dd
Graham Cox <graham@grahamcox.co.uk>
2017-08-11 13:26:05 +0100

    Support for Babel

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,54 @@ module.exports = function(grunt) {
 
   // Keep the plugins in alphabetical order
   grunt.initConfig({
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: [
+          'env'
+        ],
+        plugins: [
+          'transform-runtime'
+        ]
+      },
+      client: {
+        files: [{
+          expand: true,
+          cwd: 'src/common/javascript',
+          src: ['**/*.js'],
+          dest: 'build/client/javascript'
+        }, {
+          expand: true,
+          cwd: 'src/client/javascript',
+          src: ['**/*.js'],
+          dest: 'build/client/javascript'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: 'src/common/javascript',
+          src: ['**/*.js'],
+          dest: 'build/server'
+        }, {
+          expand: true,
+          cwd: 'src/server/javascript',
+          src: ['**/*.js'],
+          dest: 'build/server'
+        }]
+      }
+    }
   });
   
-  grunt.registerTask('build:common', [
-  ]);
-
   grunt.registerTask('build:server', [
-    'run-once:build:common'
+    'babel:server'
   ]);
   grunt.registerTask('test:server', [
     'run-once:build:server'
   ]);
 
   grunt.registerTask('build:client', [
-    'run-once:build:common'
+    'babel:client'
   ]);
   grunt.registerTask('test:client', [
     'run-once:build:client'
